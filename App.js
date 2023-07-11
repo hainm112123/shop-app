dc37f5014c4e1282a1b9464e1da5b061e62c8f2d
yuuhi <hainm112123@gmail.com>
2023-07-11 09:58:14 +0700

    fix

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import { StyleSheet, View } from 'react-native';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import axios from 'axios'
+import { SERVER_BASE_URL } from '@env';
 
 import AppNavigator from './AppNavigator'
 import { CartProvider } from './contexts/CartContext';
 
-axios.defaults.baseURL = process.env.REACT_APP_SERVER_BASE_URL;
+axios.defaults.baseURL = SERVER_BASE_URL;
 
 export default function App() {
   return (
